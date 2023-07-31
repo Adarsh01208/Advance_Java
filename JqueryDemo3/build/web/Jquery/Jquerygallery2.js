729fa120da95ec9b69a799f62bcd5ba532a74060
Adarshsingh <83551712+Adarsh01208@users.noreply.github.com>
2023-07-31 21:32:56 +0530

    Add files via upload

---
+++ JqueryDemo3/build/web/Jquery/Jquerygallery2.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ $(document).ready(function(){
+     $(".gallery_img").click(function(){
+         var img_src =$(this).attr("src");
+         var img_id=$(this).attr("id");
+         $(".main").css("display","block");
+         $(".image").css("display","block");
+       //  alert(img_id);
+          $("#show_img").attr("src", img_src);
+          
+          
+            $(".next").click(function (){
+            if(img_id === 4)
+            {
+                img_id=1;
+            }
+            else
+            {
+                img_id++;
+            }
+            
+            var new_img_id="#"+img_id;
+            var new_img_src=$(new_img_id).attr("src");
+            //alert(new_img_src);
+            $("#show_img").attr("src", new_img_src);
+        });
+        
+        $(".prev").click(function (){
+            if(img_id === 1)
+            {
+                img_id=4;
+            }
+            else
+            {
+                img_id--;
+            }
+            
+            var new_img_id="#"+img_id;
+            var new_img_src=$(new_img_id).attr("src");
+            $("#show_img").attr("src", new_img_src);
+        });
+    });
+    
+    $(".main").click(function (){
+        $(".main").css("display", "none");
+        $(".image").css("display", "none");
+     });
+   });
+
